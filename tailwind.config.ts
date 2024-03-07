c926b54425e57de1eaeed6113603cbe6f2e32ff1
Hal-ang <jpjgv05@gmail.com>
2024-03-07 23:23:58 +0900

    Set color template on tailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "light-on-background": "#1D1B20",
+        "dark-primary": "#D0BCFF",
+        "dark-on-primary": "#381E72",
+        "dark-primary-container": "#4F378B",
+        "dark-outline": "#938F99",
+        "dark-on-secondary": "#E8DEF8",
+        "light-secondary": "#625B71",
+        "dark-on-secondary-container": "#E8DEF8",
+        "dark-on-surface": "#E6E0E9",
+        "dark-surface-container-highest": "#36343B",
+        "dark-surface-container-low": "#1D1B20"
+      },
       spacing: extendedPxr,
       fontSize: extendedPxr,
       lineHeight: extendedPxr,
