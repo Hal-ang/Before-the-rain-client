4f3ed6165fef5bbee5bbfe7bff5fffd20f38353b
Hal-ang <jpjgv05@gmail.com>
2024-03-07 23:10:17 +0900

    Apply extended pxr for tailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
 import type { Config } from "tailwindcss";
 
+const { range } = require("lodash");
+
+const pxToRem = (px: number, base = 16) => `${px / base}rem`;
+
+const extendedPxr = {
+  ...range(1, 300).reduce((acc: { [key: string]: string }, px: number) => {
+    acc[`${px}pxr`] = pxToRem(px);
+    acc[`${px + 0.5}pxr`] = pxToRem(px + 0.5);
+    return acc;
+  }, {})
+};
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      spacing: extendedPxr,
+      fontSize: extendedPxr,
+      lineHeight: extendedPxr,
+      borderRadius: extendedPxr,
+      maxWidth: extendedPxr,
+      minWidth: extendedPxr,
+      minHeight: extendedPxr,
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
