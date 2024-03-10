65239c2a5eabad9006de1b530280ccab3cf1a0de
Hal-ang <jpjgv05@gmail.com>
2024-03-10 12:44:33 +0900

    Set storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,46 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling-webpack",
+      options: {
+        rules: [
+          // Replaces existing CSS rules to support PostCSS
+          {
+            test: /\.css$/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: { importLoaders: 1 }
+              },
+              {
+                // Gets options from `postcss.config.js` in your project root
+                loader: "postcss-loader",
+                options: { implementation: require.resolve("postcss") }
+              }
+            ]
+          }
+        ]
+      }
+    }
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {
+      builder: {
+        useSWC: true // Enables SWC support
+      }
+    }
+  },
+  docs: {
+    autodocs: "tag"
+  }
+};
+export default config;
