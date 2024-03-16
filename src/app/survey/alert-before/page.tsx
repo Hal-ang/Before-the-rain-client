c2ae2bb4e94dc1cddf35be6aaf54910aa5d64091
Hal-ang <jpjgv05@gmail.com>
2024-03-17 01:37:26 +0900

    Refactor alert-before page

--- src/app/survey/alert-before/page.tsx
+++ src/app/survey/alert-before/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from "react";
 
 import BackHeader from "@/components/header/BackHeader";
 import FadeTitle from "@/components/FadeTitle";
@@ -11,27 +17,17 @@ import TransitionTightSection from "@/components/layout/TransitionTightSection";
 import { surveyAtom } from "@/atom/survey";
 import { useAtom } from "jotai";
 import useFocused from "@/hooks/useFocused";
-import useNextSurvey from "@/hooks/survey/useNextSurvey";
+import { useRouter } from "next/navigation";
 import useSurveyProgressPercent from "@/hooks/survey/useSurveyProgressPercent";
 
 const AlertBefore = () => {
+  const router = useRouter();
   const hourRef = useRef<HTMLInputElement>(null);
   const minuteRef = useRef<HTMLInputElement>(null);
-  const [isDone, setIsDone] = useState(false);
 
   const [{ alertBeforeRain }, setSurvey] = useAtom(surveyAtom);
 
-  useEffect(() => {
-    if (!alertBeforeRain || !hourRef.current || !minuteRef.current) return;
-
-    const hour = Math.floor(alertBeforeRain / 60);
-    const minute = alertBeforeRain % 60;
-
-    hourRef.current.value = String(hour);
-    minuteRef.current.value = String(minute);
-
-    setIsDone(hour + minute > 0);
-  }, [alertBeforeRain, hourRef, minuteRef]);
+  const [time, setTime] = useState(() => alertBeforeRain);
 
   const { isFocused: isHourFocused, bind: hourBind } = useFocused();
   const { isFocused: isMinuteFocused, bind: minuteBind } = useFocused();
@@ -40,22 +36,12 @@ const AlertBefore = () => {
     () => isHourFocused || isMinuteFocused,
     [isHourFocused, isMinuteFocused]
   );
-
-  useEffect(() => {
-    if (isFocused) return;
-
-    const hour = Number(hourRef.current?.value);
-    const minute = Number(minuteRef.current?.value);
-    const value = hour * 60 + minute;
-    setIsDone(value > 0);
-  }, [isFocused]);
-
-  const percent = useSurveyProgressPercent(isDone);
+  const isDone = useMemo(() => time > 0 && !isFocused, [isFocused, time]);
 
   const [shouldTransition, setShouldTransition] = useState(false);
 
   useEffect(() => {
-    if (isDone) return;
+    if (alertBeforeRain) return;
 
     const timeoutID = setTimeout(() => {
       setShouldTransition(true);
@@ -66,25 +52,15 @@ const AlertBefore = () => {
   }, []);
 
   useEffect(() => {
-    if (!isDone) return;
+    setShouldTransition(isFocused);
+  }, [isFocused]);
 
-    const timeoutID = setTimeout(() => {
-      setShouldTransition(false);
-    }, 200);
-    return () => {
-      clearTimeout(timeoutID);
-    };
-  }, [isDone]);
+  const onClickNext = useCallback(() => {
+    setSurvey({ alertBeforeRain: time });
+    router.push("/survey/alert-summary/agreed");
+  }, [time]);
 
-  const { goToNextPage } = useNextSurvey();
-  const onClickNext = () => {
-    const hour = Number(hourRef.current?.value);
-    const minute = Number(minuteRef.current?.value);
-    const value = hour * 60 + minute;
-    setSurvey({ alertBeforeRain: value });
-
-    goToNextPage();
-  };
+  const percent = useSurveyProgressPercent(isDone);
 
   return (
     <main className="w-full flex flex-col min-h-screen">
@@ -98,6 +74,7 @@ const AlertBefore = () => {
         Top={<FadeTitle text="미리 알려 드릴게요" />}
         Bottom={
           <TimeInput
+            useTime={[time, setTime]}
             hourRef={hourRef}
             minuteRef={minuteRef}
             hourBind={hourBind}
