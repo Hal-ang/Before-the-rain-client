0822c83254431d0fdea006f183a62f066efbd503
Hal-ang <jpjgv05@gmail.com>
2024-03-23 17:33:42 +0900

    Simplify webview atoms

--- src/atom/webview.ts
+++ src/atom/webview.ts
@@ -1,6 +1,10 @@
 import { atom } from "jotai";
 
-export const userCoordinatesAtom = atom({ lat: 37.59996944, lon: 126.9312417 });
-export const enabledLocationPermissionAtom = atom(false);
+export const userCoordinatesAtom = atom({
+  lat: 0,
+  lon: 0,
+  updatedAt: 0
+});
 export const enabledNotificationPermissionAtom = atom(false);
+
 export const FCMTokenAtom = atom("");
