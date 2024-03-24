b6089dff8313477f35bbc672bd71b854492bbba7
Hal-ang <jpjgv05@gmail.com>
2024-03-24 15:38:17 +0900

    Implement LargeInput interaction test

--- src/stories/LargeInput.stories.tsx
+++ src/stories/LargeInput.stories.tsx
@@ -1,3 +1,4 @@
+import { userEvent, waitFor, within, expect, fn } from "@storybook/test";
 import { Meta, StoryObj } from "@storybook/react";
 
 import { LinearButton } from "@/components/button/LinearButton";
@@ -20,5 +21,35 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     placeholder: "00"
+  },
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await step("값 입력", async () => {
+      await userEvent.type(canvas.getByTestId("largeInput"), "32");
+    });
+  }
+};
+
+export const Number: Story = {
+  args: {
+    type: "number",
+    inputMode: "numeric" as "numeric",
+    pattern: "[0-9]*",
+    placeholder: "00"
+  },
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    const largeInput = canvas.getByTestId("largeInput");
+    await step("숫자가 아닌 값 입력", async () => {
+      await userEvent.type(largeInput, "테스트입니다");
+      await waitFor(() => expect(largeInput).toHaveDisplayValue(""));
+    });
+
+    await step("숫자 입력", async () => {
+      await userEvent.type(largeInput, "3424234");
+      await waitFor(() => expect(largeInput).toHaveDisplayValue("3424234"));
+    });
   }
 };
