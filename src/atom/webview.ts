386c7533e94d75a835e502da109773c449104f46
Hal-ang <jpjgv05@gmail.com>
2024-03-26 20:02:38 +0900

    Handle permission degreed user

--- src/atom/webview.ts
+++ src/atom/webview.ts
@@ -1,9 +1,8 @@
 import { atom } from "jotai";
 
 export const userCoordinatesAtom = atom({
-  // TODO : 심사 통과 시 0으로 수정 예정
-  lat: 37.59996944,
-  lon: 126.9312417,
+  lat: 0,
+  lon: 0,
   updatedAt: 0
 });
 export const enabledNotificationPermissionAtom = atom(false);
