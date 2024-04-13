36e82ee6a2af962c92d60cb71a1a3d45537c2e64
Hal-ang <68503014+Hal-ang@users.noreply.github.com>
2024-04-14 01:16:52 +0900

    Set initiali sentry (#17)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import * as Sentry from "@sentry/react";
+
 import AuthGuard from "@/components/auth/AuthGuard";
 import MSW from "@/components/\bmsw";
 import { MY_FONTS } from "@/styles/fonts";
@@ -7,6 +9,21 @@ import type { Metadata } from "next";
 import Providers from "../components/providers";
 import StyledComponentsRegistry from "../../lib/registry";
 import Webview from "@/components/Webview";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 export const metadata: Metadata = {
   title: "Before the rain",
